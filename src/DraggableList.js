ada144b10c96e886f8e66121b7265d958a49ff84
ank0409 <ankitgandhi.me@gmail.com>
2023-01-12 16:16:31 -0500

    3 files modified

--- src/DraggableList.js
+++ src/DraggableList.js
@@ -2,15 +2,37 @@ import React, { useState } from 'react';
 import Form from './Form';
 
 const DraggableList = ({ sendtags }) => {
+  const [testval, settestval] = useState('');
   return (
     <div>
       <hr />
       <h1>List items will be added here.</h1>
+      {/* {sendtags.map((x) => {
+        return (
+          <>
+            {x.closingtagsend.map((x) => {
+              if (x == 'true') {
+                return (
+                  // <>{x}</>
+                  sendtags.map((x) => {
+                    return (
+                      <>
+                        {x.syntaxgtagsend.map((x) => {
+                          return <li>{x}</li>;
+                        })}
+                      </>
+                    );
+                  })
+                );
+              }
+            })}
+          </>
+        );
+      })} */}
 
-      {sendtags.map((x) => {
-        return <>{x.syntaxgtagsend}</>;
-      })}
-
+      {/* {x.syntaxgtagsend.map((x) => {
+              return <li>{x}</li>;
+            })} */}
       {/* {sendtags.syntaxgtagsend.map((syntax, index) => {
         return <li key={index}>{syntax}</li>;
       })} */}
