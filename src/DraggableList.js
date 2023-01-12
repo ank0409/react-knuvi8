d971e9be97255dc55f444a1c45f9c50d072f53c0
ank0409 <ankitgandhi.me@gmail.com>
2023-01-12 14:00:29 -0500

    1 file modified

--- src/DraggableList.js
+++ src/DraggableList.js
@@ -6,40 +6,10 @@ const DraggableList = ({ sendtags }) => {
     <div>
       <hr />
       <h1>List items will be added here.</h1>
-      {/* {items.map((x, index) => {
-        return (
-          <li key={index}>
-            {x}
-            <Form />
 
-            {'>'}
-            <br />
-            <input placeholder={x + 'name'} />
-            <br />
-            {x + '>'}
-            <button>+add more atts +</button>
-          </li>
-        );
-      })} */}
-
-      {/* {senddata.map((x) => {
-        return <li>{x.openingtag}</li>;
-      })} */}
-      {/* {
-        senddata.openingtag.map((x)=>{
-          return <li>{senddata.openingtag}</li>
-        })
-      } */}
-
-      {/* <li>Opening Tag: {senddata.openingtag}</li>
-      <li>Closing Tag: {senddata.closingtag}</li> */}
-
-      {sendtags.syntaxgtagsend.map((x, index) => {
-        return <li key={index}>{x}</li>;
+      {sendtags.syntaxgtagsend.map((syntax, index) => {
+        return <li key={index}>{syntax}</li>;
       })}
-      {/* {sendtags.closingtagsend.map((x, index) => {
-        return <li key={index}>{x}</li>;
-      })} */}
     </div>
   );
 };
